5685322331eb15e326aa4848343d4eb9484cf619
Bakhytgali <129316187+Bakhytgali@users.noreply.github.com>
2023-04-18 21:15:43 +0600

    Update MyArrayList.java

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -2,6 +2,7 @@ public class MyArrayList implements MyList{
     private int[] arr;
     private int size = 0;
     private int capacity = 5;
+    @Override
     public int size(){
         return size;
     }
@@ -10,5 +11,12 @@ public class MyArrayList implements MyList{
             increaseLength();
         }
         arr[size++] = el;
+    }
+    public void increaseLength(){
+        capacity *= 2;
+        int[] arr2 = new int[capacity];
+        for(int i = 0; i < size; i++) {
+            arr2[i] = arr[i];
+        }
     }
 }
