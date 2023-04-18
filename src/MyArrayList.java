77f60b825b5b1cee3a7a48c7e1df79c694bdb4be
Your Name <omgwtfwat@gmail.com>
2023-04-18 19:29:15 +0600

    add function

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,2 +1,14 @@
-public class MyArrayList {
+public class MyArrayList implements MyList{
+    private int[] arr;
+    private int size = 0;
+    private int capacity = 5;
+    public int size(){
+        return size;
+    }
+    public void add(int el){
+        if(size == capacity){
+            increaseLength();
+        }
+        arr[size++] = el;
+    }
 }
