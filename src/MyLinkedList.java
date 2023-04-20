6a933c4b796e37c73127cd9ebb9b55b1b672f808
Bakhytgali <129316187+Bakhytgali@users.noreply.github.com>
2023-04-20 20:29:31 +0600

    Update MyLinkedList.java

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,4 +1,12 @@
 public class MyLinkedList<T> implements MyList {
+  class MyNode<T>{
+        int data;
+        MyNode next;
+        MyNode(int data){
+            this.data = data;
+            next = null;
+        }
+    }
   @Override
     public void get(){
 
